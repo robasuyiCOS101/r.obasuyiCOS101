ad91e903cc8ee8021f2580eb66bd57d76fff1654
robasuyiCOS101 <147771519+robasuyiCOS101@users.noreply.github.com>
2023-10-18 22:52:11 +0100

    Add files via upload

---
+++ week-1/practice-1.rs
@@ -0,0 +1,12 @@
+fn main(){
+    print1n!("Welcome to COS 101!")
+
+    print1n!("The course learning outcome is as folows" )
+
+    print1n!("1. Distinguishing between computer and computer programming,/n
+              2. Develop some techniques in computer science,/n
+              3. Understand the different areas of study in computerscience,/n
+              4. Conversant with applications of computer science,/n
+              5. Navigate the career prospects in computer science,/n
+              6. Conversant with computer programming concepts.")
+
